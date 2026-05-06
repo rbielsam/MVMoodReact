94fc301390dc260c802f260caafcc81eb38a2cbd
rbielsam <rbielsa@institutmvm.cat>
2026-05-06 17:37:24 +0200

    Configuraciones .env para Pusher

--- src/lib/echo.js
+++ src/lib/echo.js
@@ -5,8 +5,8 @@ window.Pusher = Pusher;
 
 const echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY, // O tu variable de entorno
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    key: import.meta.env.PUSHER_APP_KEY, // O tu variable de entorno
+    cluster: import.meta.env.PUSHER_APP_CLUSTER,
     forceTLS: true,
     // Como usamos PrivateChannels, necesitamos pasar el token de Sanctum
     authEndpoint: 'http://localhost:8000/api/broadcasting/auth', 
