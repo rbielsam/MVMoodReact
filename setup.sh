3e9e40c57ef09f9162c09dd05a8e915ff8e53335
Patricio Salazar <patricio.victor.2005@gmail.com>
2026-05-14 17:10:58 +0200

    Test setup front-end

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "VITE_PUSHER_APP_ID=2143530" >> .env
+echo "VITE_PUSHER_APP_KEY=fc768dea3d34d2690a2f" >> .env
+echo "VITE_PUSHER_APP_SECRET=97c14b40d5644ab640a9" >> .env
+echo "VITE_PUSHER_APP_CLUSTER=eu" >> .env
+echo "API_URL=http://127.0.0.1:8000" >> .env
+npm install
+npm run dev
